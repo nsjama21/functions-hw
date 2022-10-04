b87bd331e192174ad8ce0eea10cce10f19bc6094
nsjama15 <nsjama21@gmail.com>
2022-10-04 18:52:15 -0400

    first function

---
+++ functions-hw.js
@@ -0,0 +1,13 @@
+// 1. Function 
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+        return x;
+    } else {
+        return y;
+    }
+
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+}
+
+console.log(maxOfTwoNumbers(3, 9));
