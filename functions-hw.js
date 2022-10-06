7fa6f359bd51d597b45986e8d5851ccbe02bd19f
nsjama15 <nsjama21@gmail.com>
2022-10-06 18:01:28 -0400

    eight/ninth function and edits after Christina helped me

--- functions-hw.js
+++ functions-hw.js
@@ -43,32 +43,32 @@ console.log(isCharAVowel("a"));
 
 // 4. Function
 // Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
+// num is what you call each element in array and base+=num is what you do to it
 
 
-
-const sumArray = (...args) => {
+const sumArray = (inputArray) => {
     let base = 0;
-    args.forEach((num) => {
+    inputArray.forEach((num) => {
         base += num
     })
     return base
 }
 
-console.log(sumArray(2, 4, 5))
+console.log(sumArray([2, 4, 5]))
 
 
 // 5. Function
 // Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product of those numbers. For example, multiplyArray([2, 4, 5]);would return 40
 
-function multiplyArray(...args) {
+function multiplyArray(inputArrayOne) {
     let baseTwo = 1;
-    args.forEach((num) => {
+    inputArrayOne.forEach((num) => {
         baseTwo *= num
     })
     return baseTwo
 }
 
-console.log(multiplyArray(2, 4, 5))
+console.log(multiplyArray([2, 4, 5]))
 
 // 6. Function
 // Define a function, as a function expression, numArgsthat returns the number of arguments passed to the function when called.
@@ -90,3 +90,39 @@ function reverseString(str) {
 
 reverseString("Nimo");
 
+// 8. Function
+// Define a function, as a function expression, longestStringInArraythat takes an array of strings as an argument and returns the length of the longest string. // an array is really just one thing
+
+const longestStringInArray = (stringArray) => {
+    longestString = 0;
+    for (let i = 0; i < stringArray.length; i++) {
+        if (stringArray[i].length > longestString) {
+            longestString = stringArray[i].length
+        }
+    }
+
+    return longestString;
+
+}
+
+const final = longestStringInArray(["words", "chicken", "livelihood"])
+console.log(final)
+
+
+// 9. Function
+
+// Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments, and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"]. // two arguments one is an array and one is a number
+
+function stringsLongerThan(stringArrayOne, strLength) {
+    const newArray = [];
+    for (let i = 0; i < stringArrayOne.length; i++) {
+        if (stringArrayOne[i].length > strLength) {
+            newArray.push(stringArrayOne[i])
+        }
+    }
+
+    return newArray;
+
+}
+
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 4))
